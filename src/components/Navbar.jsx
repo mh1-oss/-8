683a156bb5429928c699e93b08e6ca6ada56c58e
mh1-oss <mmhhoo967.mm@gamil.com>
2026-01-31 03:39:06 +0300

    e3

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,25 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-
+import { useScroll, useMotionValueEvent } from 'framer-motion';
 const Navbar = ({ cartCount }) => {
     const location = useLocation();
     const isRecipePage = location.pathname.startsWith('/meal/');
-    console.log("Navbar check:", location.pathname, "isRecipePage:", isRecipePage);
+    const [isTransparent, setIsTransparent] = useState(true);
+    const { scrollY } = useScroll();
 
+    // Monitor scroll
+    useMotionValueEvent(scrollY, "change", (latest) => {
+        // إذا نزل المستخدم أكثر من 50 بكسل، يصبح الـ Navbar ملوناً
+        if (latest > 50) {
+            setIsTransparent(false);
+        } else {
+            setIsTransparent(true);
+        }
+    });
     return (
-        <nav className={`navbar ${isRecipePage ? 'transparent-navbar' : ''}`}>
+        // Only apply transparency if we are on a Recipe Page AND at the top
+        <nav className={`navbar ${isRecipePage && isTransparent ? 'transparent-navbar' : ''}`}>
             <div className="logo">
                 <Link to="/" style={{ textDecoration: 'none', color: 'inherit' }}>
                     Crave<span style={{ color: 'var(--primary-color)' }}>Find</span> <img src="/restaurant_menu.png" alt="" />
